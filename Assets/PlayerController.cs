272e38933168d75555efdbb6edd32545cc9a8148
Gwon Tae Hyeon <85162769+xogus4570@users.noreply.github.com>
2025-09-09 21:53:16 +0900

    상호작용(F)구현

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -23,14 +23,25 @@ public class PlayerController : MonoBehaviour
     {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
-        input = input.sqrMagnitude > 1f ? input.normalized : input;
+        input = input.sqrMagnitude > 1f ? input.normalized : input;// 대각선 이동시 속도 보정 input.sqrMagnitude<- 벡터 길이 제곱
 
 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Collider2D target = Physics2D.OverlapCircle(transform.position, interactRadius, interactableMask);
+            if (target != null)
+            {
+                target.GetComponent<CraftingTable>()?.OnInteract(); //?앞에가 null이 아니면 함수 실행, ? 앞이 null이면 null 반환
+                //제작대 코드로가서 ui생성하는 코드로 바꿔서 만들면 될 듯
+
+            }
+
+        }
+       
     }
 
     void FixedUpdate()
     {
         rb.velocity = input * moveSpeed;
     }
-
 }
